d65e7e56ae73a30ac75acb591d7c0ee987a02fde
Kimia Heidari <kimia_heidari1998@outlook.com>
2025-05-29 00:53:51 +0330

    +Edit

---
+++ EvenDrivenArchitecture/Program.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddMassTransit(configurator =>
+{
+    configurator.AddBus(provider => Bus.Factory.CreateUsingRabbitMq(cfg =>
+    {
+
+        cfg.Host(new Uri($"rabbitmq://localhost/"), h =>
+        {
+            h.Username("guest");
+            h.Password("guest");
+        });
+
+    }));
+});
+var serviceProvider = builder.Services.BuildServiceProvider();
+var bus = serviceProvider.GetRequiredService<IBusControl>();
+await bus.StartAsync();
